79d33c807c813ab4c26562b454854ea9a595f0b5
Lavmee <66745577+Lavmee@users.noreply.github.com>
2022-03-20 03:12:58 +0300

    feat: Creating a Glide Module.

---
+++ app/src/main/java/com/annexflow/calculatorannex/presentation/base/util/AnnexAppGlideModule.kt
@@ -0,0 +1,11 @@
+package com.annexflow.calculatorannex.presentation.base.util
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+/**
+ * Created by Samuel Nash on 20.03.2022.
+ */
+
+@GlideModule
+class AnnexAppGlideModule : AppGlideModule()
